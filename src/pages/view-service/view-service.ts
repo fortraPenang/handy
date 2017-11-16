2699776f161f1e11f30b9f6bc31962ad18398980
Sriram97 <sriram6897@gmail.com>
2017-11-16 18:49:31 +0800

    distance error

--- src/pages/view-service/view-service.ts
+++ src/pages/view-service/view-service.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { AngularFireDatabase } from "angularfire2/database";
 import { Observable } from 'rxjs/observable';
+import { NgZone } from '@angular/core';
 import firebase from 'firebase';
 
 declare var google;
@@ -25,35 +26,132 @@ export class ViewServicePage {
   obj: { Address: string,
          Category: string};
   map: any;
+  public curLat:any;
+  public curLng:any;
 
-  public handys: any;
+  
+  public vndLat: number[] = [];  
+  public vndLng:number[] = []; 
+
+  public distance: number[] = [];
+  public vndAddress: string[] = [];
+
+  public handys:any;
   database = firebase.database();
-  valueRef = firebase.database().ref('/Handys/');
+  valueRef = firebase.database().ref('/Handys/vendor');
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private geolocation: Geolocation) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private geolocation: Geolocation, private zone: NgZone) {
     
   }
 
+  setAddressArray(callback){
+    for(var i=0;i<this.handys.length;i++){
+      var add= this.handys[i]['addressLine1']+" "+this.handys[i]['addressLine2']+" "+this.handys[i]['addressLine3']+" "+this.handys[i]['postal_code']+" "+this.handys[i]['city']+" "+this.handys[i]['state'];
+      this.vndAddress.push(add);
+      //this.codeAddress(add);
+    }
+    console.log("Set Address Array done");
+    console.log(this.vndAddress);
+    callback();
+  }
   
+
+  addToCoords(){
+    for(var i=0;i<this.vndAddress.length;i++){
+
+    }
+  }
+
+  geocoder = new google.maps.Geocoder();
   
+  codeAddress(add,callback) {
+    
+      //In this case it gets the address from an element on the page, but obviously you  could just pass it to the method instead
+      for(var i=0;i<add.length;i++){
+        var address = add[i];
+        var tempLat;
+        var tempLng;
+    
+        this.geocoder.geocode( { 'address' : address }, ( results, status ) => {
+            if( status == google.maps.GeocoderStatus.OK ) {
+    
+                //In this case it creates a marker, but you can get the lat and lng from the location.LatLng
+                debugger;
+                var lat = results[0].geometry.location.lat();
+                var lng = results[0].geometry.location.lng();
+                this.vndLat.push(lat);
+                this.vndLng.push(lng);
+  
+                let marker = new google.maps.Marker({
+                  map: this.map,
+                  animation: google.maps.Animation.DROP,
+                  position: results[0].geometry.location
+                });
+             } else {
+                alert( 'Geocode was not successful for the following reason: ' + status );
+            }
+        } );
+      }
+      callback();
+      
+  }
+
+  deg2rad(deg) {
+    return deg * (Math.PI/180)
+  }
+
+  getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = this.deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = this.deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+   } 
+
+    setDistance(){
+     var i;
+     var self = this;
+     console.log(this.vndLat.length);
+     for(i=0;i<this.vndLat.length;i++){
+       var d = this.getDistanceFromLatLonInKm(this.curLat,this.curLng,this.vndLat[i],this.vndLng[i])
+       this.distance.push(d);
+       console.log(d);
+     }
+   } 
+  
+ 
   
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ViewServicePage');
+    this.valueRef.on('value', handy => {
+      this.handys=handy.val();
+      this.setAddressArray(()=>{
+        this.codeAddress(this.vndAddress,()=>{
+          this.setDistance();
+        });
+      });
+    });
     this.loadMap();
-    this.valueRef.on('value', function(handy) {
-      //this.handys = handy.val();
-      console.log(handy.val()[0]['Address']);
-    });  
+    console.log(this.distance);
+      
   }
+  
 
   loadMap(){
   this.geolocation.getCurrentPosition().then((resp) =>{
-    console.log(resp.coords.latitude)
-    console.log(resp.coords.longitude)
+
+    this.curLat=resp.coords.latitude;
+    this.curLng=resp.coords.longitude;
+    //this.getDistanceFromLatLonInKm(resp.coords.latitude,resp.coords.longitude,resp.coords.latitude,resp.coords.longitude);
     let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
 
-  
 
     let mapOptions = {
       /*camera: {
@@ -74,12 +172,21 @@ export class ViewServicePage {
       animation: google.maps.Animation.DROP,
       position: latLng
     });
+
+    for (var i = 0; i < this.vndLat.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(this.vndLat[i], this.vndLng[i]),
+        map: this.map
+      });
+    }
+    
     }).catch((error)=>{
         console.log('Error getting location',error);
       });
 
       
   }
+   
   addMarker(){
     
      let marker = new google.maps.Marker({
