d62778f5d3e1dff6c854141b00f41bbed294dc2c
pepe moe <muhammad24lokman@gmail.com>
2017-11-17 08:03:38 +0800

    removes conflict

--- src/pages/user-login/user-login.ts
+++ src/pages/user-login/user-login.ts
@@ -1,25 +1,16 @@
 import { Component } from '@angular/core';
 // import UI controllers 
-<<<<<<< HEAD
-import { IonicPage, LoadingController, NavController, ToastController, ModalController, NavParams, AlertController } from 'ionic-angular';
-=======
 import { IonicPage, LoadingController, NavController, ToastController, ModalController, NavParams, AlertController , MenuController} from 'ionic-angular';
->>>>>>> 95a965fe16b68332ef3888883401ad327299e322
 import { Dashboard } from '../dashboard/dashboard';
 import { UserSignup } from '../user-signup/user-signup';
 import { UserForgotpassword } from '../user-forgotpassword/user-forgotpassword';
 import { AuthService } from '../../providers/auth-service';
-<<<<<<< HEAD
-// form builder and validators
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-=======
 import { GooglePlus } from '@ionic-native/google-plus';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireModule } from 'angularfire2';
 // form builder and validators
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as firebase from 'firebase/app';
->>>>>>> 95a965fe16b68332ef3888883401ad327299e322
 
 @IonicPage()
 @Component({
@@ -27,17 +18,6 @@ import * as firebase from 'firebase/app';
   templateUrl: 'user-login.html',
 })
 export class UserLogin {  
-<<<<<<< HEAD
- account: { email: string, password: string } = {
-    email: '',
-    password: ''
-  };
-  public loginForm :any;
-  submitAttempt: boolean = false;  
- constructor(public navCtrl: NavController,
-    public navParams: NavParams,
-    public authService: AuthService,
-=======
   account: { email: string, password: string } = {
     email: '',
     password: ''
@@ -47,19 +27,10 @@ export class UserLogin {
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     public authService: AuthService, 
->>>>>>> 95a965fe16b68332ef3888883401ad327299e322
     public toastCtrl: ToastController,
     public modalCtrl: ModalController,
     public loadingCtrl: LoadingController,
     public alertCtrl: AlertController,
-<<<<<<< HEAD
-    public builder: FormBuilder) {
-
-   this.loginForm = builder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-=======
     public builder: FormBuilder,
     public googlePlus: GooglePlus,
     public afAuth: AngularFireAuth,
@@ -70,7 +41,6 @@ export class UserLogin {
       password: ['', Validators.required]
     });
 
->>>>>>> 95a965fe16b68332ef3888883401ad327299e322
   }
 
  ionViewDidLoad() {
@@ -83,11 +53,7 @@ export class UserLogin {
   forgotPasswordPage(){ this.navCtrl.push(UserForgotpassword); }
 
 
-<<<<<<< HEAD
- doLogin(){
-=======
   doLogin(){
->>>>>>> 95a965fe16b68332ef3888883401ad327299e322
     this.submitAttempt = true;
     let loader = this.loadingCtrl.create({
       dismissOnPageChange: true,
@@ -97,10 +63,7 @@ export class UserLogin {
       this.authService.login(this.account).then(authData => {
         //successful
         console.log(authData);
-<<<<<<< HEAD
-=======
         this.menuCtrl.swipeEnable(true);
->>>>>>> 95a965fe16b68332ef3888883401ad327299e322
         this.navCtrl.setRoot(Dashboard);
       }, error => {
         console.log(error);
@@ -109,11 +72,7 @@ export class UserLogin {
         //custom error message
         if (errorCode == "auth/user-disabled" ||
             errorCode == "auth/user-not-found" || errorCode == "auth/wrong-password") errorMsg = "Wrong username or password.";
-<<<<<<< HEAD
-        if(errorCode == "auth/invalid-email") errorMsg = "Please provide email with the correct format.";
-=======
         if(errorCode == "auth/invalid-email") errorMsg = "Please provide email with the correct format."; 
->>>>>>> 95a965fe16b68332ef3888883401ad327299e322
         let alert = this.alertCtrl.create({
           title: "Login Failed",
           subTitle: errorMsg,
@@ -125,13 +84,9 @@ export class UserLogin {
       loader.present();
     }
     
-<<<<<<< HEAD
- }
-}
-=======
   }
 
-  signInGoogle(){
+  /* signInGoogle(){
     let loader = this.loadingCtrl.create({
       dismissOnPageChange: true,
     });
@@ -153,6 +108,5 @@ export class UserLogin {
         loader.dismiss();
       })
     })
-  }
+  } */
 }
->>>>>>> 95a965fe16b68332ef3888883401ad327299e322
--- src/providers/auth-service.ts
+++ src/providers/auth-service.ts
@@ -2,35 +2,23 @@ import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireModule } from 'angularfire2';
-<<<<<<< HEAD
-import { Dashboard } from '../pages/dashboard/dashboard';
-// Do not import from 'firebase' as you'll lose the tree shaking benefits
-import * as firebase from 'firebase/app';
-import { GooglePlus } from '@ionic-native/google-plus';
-import { IonicPage, LoadingController, NavController, ToastController, ModalController, NavParams, AlertController } from 'ionic-angular';
-=======
 import { GooglePlus } from '@ionic-native/google-plus';
 import { IonicPage, LoadingController, ToastController, ModalController, NavParams, AlertController } from 
 'ionic-angular';
 import { Dashboard } from '../pages/dashboard/dashboard';
 // Do not import from 'firebase' as you'll lose the tree shaking benefits
 import * as firebase from 'firebase/app';
->>>>>>> 95a965fe16b68332ef3888883401ad327299e322
 
 @Injectable()
 export class AuthService {
   private currentUser: firebase.User;
 
-<<<<<<< HEAD
-  constructor(public alertCtrl: AlertController,public loadingCtrl: LoadingController,public navCtrl: NavController,public afAuth: AngularFireAuth, public googlePlus:GooglePlus) {
-=======
   constructor(public afAuth: AngularFireAuth, 
     public googlePlus: GooglePlus, 
     public toastCtrl: ToastController,
     public modalCtrl: ModalController,
     public loadingCtrl: LoadingController,
     public alertCtrl: AlertController,) {
->>>>>>> 95a965fe16b68332ef3888883401ad327299e322
     afAuth.authState.subscribe((user: firebase.User) => this.currentUser = user);
   }
 
@@ -48,36 +36,9 @@ export class AuthService {
   }
 
   //TODO:
-<<<<<<< HEAD
-  async loginGoogle(accountInfo: any){
-      let loader = this.loadingCtrl.create({
-        dismissOnPageChange: true,
-      });
-      this.googlePlus.login({
-        'webClientId' : '447284265080-llk2rv349uf9lv2iah4oiftuq6secopg.apps.googleusercontent.com',
-        'offline' : true,  
-     }).then(res => {
-        firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken))
-        .then(suc => {
-          console.log(suc);
-          this.navCtrl.setRoot(Dashboard);
-        }).catch(err =>{
-          let alert = this.alertCtrl.create({
-            title: "Login Failed",
-            subTitle: err,
-            buttons: ['Confirm']
-          });
-          alert.present();
-          loader.dismiss();
-        })
-      })
-      loader.present();
-  }
-=======
   async loginGoogle(){
 
     }
->>>>>>> 95a965fe16b68332ef3888883401ad327299e322
 
   //TODO:
   async loginFacebook(accountInfo: any){
